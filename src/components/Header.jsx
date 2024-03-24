c398f9f0d0f02cbc4106e0c82c31ad75a4851ea4
GaneshMumbai2001 <103201642+GaneshMumbai2001@users.noreply.github.com>
2024-03-24 16:15:35 +0530

    create components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <Navbar className='border-b-2'>
+        <Link to="/" className='self-center'>
+          <span>Ganesh's </span>
+          Blog
+        </Link>
+    </Navbar>
+  )
+}
